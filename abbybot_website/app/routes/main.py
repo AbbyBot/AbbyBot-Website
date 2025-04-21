1d39c851c5e5a2228e7c476bbdbe30c645360a13
Marcos Reyes <astronautmarkus@gmail.com>
2025-04-20 20:33:47 -0400

    feat: add multilingual support by implementing language-specific index templates

--- abbybot_website/app/routes/main.py
+++ abbybot_website/app/routes/main.py
@@ -5,13 +5,20 @@ from ..utilities.db_connections import execute_query
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-def index():
+@main_bp.route('/<lang_code>/')
+def index(lang_code=None):
     try:
         server_list = execute_query("discord", "SELECT FORMAT(counter, 0) AS counter, FORMAT(full_members, 0) AS full_members, FORMAT(total_xp, 0) AS total_xp FROM (SELECT COUNT(guild_id) AS counter, SUM(member_count) AS full_members FROM server_settings) AS ss, (SELECT SUM(xp_total) AS total_xp FROM user_profile) AS up;")
         server_count = server_list[0]['counter'] if server_list and server_list[0]['counter'] is not None else "no data available"
         total_members = server_list[0]['full_members'] if server_list and server_list[0]['full_members'] is not None else "no data available"
         total_xp = server_list[0]['total_xp'] if server_list and server_list[0]['total_xp'] is not None else "no data available"
-        return render_template('index.html', server_count=server_count, total_members=total_members, total_xp=total_xp)
+
+        if lang_code in ['en', 'es']:
+            template_name = f'index_{lang_code}.html'
+        else:
+            template_name = 'index.html'
+
+        return render_template(template_name, server_count=server_count, total_members=total_members, total_xp=total_xp)
 
     except mysql.connector.Error as err:
         error_message = "We are currently unable to load the AbbyBot data. Please try again later."
